dcff1e3930ac365a22bd7c7e89521718b5d60517
Simon Wisdom <swisdom@squareup.com>
2023-02-01 21:04:52 -0800

    Updates

---
+++ precache-manifest.f7347bdf5fd533221e070491d500602c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f243de7c91b1797341b46229f25bb0e9",
+    "url": "/kitchen-food-ordering/index.html"
+  },
+  {
+    "revision": "2f28f1cd7772bd2c6b0d",
+    "url": "/kitchen-food-ordering/static/css/main.67eb5f6b.chunk.css"
+  },
+  {
+    "revision": "647bc1a29a8736be03c7",
+    "url": "/kitchen-food-ordering/static/js/2.adcb9023.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/kitchen-food-ordering/static/js/2.adcb9023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f28f1cd7772bd2c6b0d",
+    "url": "/kitchen-food-ordering/static/js/main.71ebb58f.chunk.js"
+  },
+  {
+    "revision": "d3c1ee91cd0748f9ac8e",
+    "url": "/kitchen-food-ordering/static/js/runtime-main.abb04edf.js"
+  }
+]);
