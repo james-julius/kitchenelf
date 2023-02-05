a79545f6112f382039a293c76675e95afe59c9cb
Simon Wisdom <swisdom@squareup.com>
2023-02-05 12:31:56 -0800

    Updated with Chakra UI + items in Airtable

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,33 +1,36 @@
-import { Card, CardHeader, Heading, CardBody, Stack, StackDivider, Box, Text, Image } from '@chakra-ui/react'
+import {  Card, CardHeader, Heading, Image } from '@chakra-ui/react'
+// import { useDisclosure, PopoverHeader, Card, CardHeader, Heading, Image , Popover, PopoverTrigger, PopoverArrow, PopoverContent, PopoverCloseButton, PopoverBody, Button} from '@chakra-ui/react'
 
-const ItemCard = ({item, ...rest}) => {
+
+const ItemCard = ({item, handleConfirm, ...rest}) => {
+
+// const { isOpen, onToggle, onClose } = useDisclosure()
 
 return (
-<Card cursor='pointer' maxWidth={'400px'} {...rest}>
-<Image
-      src={item.image}
-      borderRadius='lg'
-    />
-
-  <CardHeader>
-    <Heading size='md'>{item.title}</Heading>
-  </CardHeader>
-
-  {/* <CardBody>
-    <Stack divider={<StackDivider />} spacing='4'>
-
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Analysis
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          See a detailed analysis of all your business clients.
-        </Text>
-      </Box>
-    </Stack>
-  </CardBody> */}
-</Card>
-
+  // <Popover>
+  //    <PopoverTrigger>
+    <Card cursor='pointer' maxWidth={'400px'} {...rest}>
+    <Image
+          // maxHeight={'200px'}
+          src={item.fields.image}
+          borderRadius='lg'
+        />
+      <CardHeader>
+        <Heading size='md'>{item.fields.name}</Heading>
+      </CardHeader>
+    </Card>
+//     </PopoverTrigger>
+//     <PopoverContent>
+//     <PopoverArrow />
+//     {/* <PopoverCloseButton /> */}
+//     <PopoverHeader>Hi, I'm the Kitchen Elf!</PopoverHeader>
+//     <PopoverBody>
+//       <p>Are you sure you want to order {item.fields.name}?</p>
+//           <Button onClick={handleConfirm}>Yes</Button>
+//           <Button onClick={onClose}>No</Button>
+//     </PopoverBody>
+//   </PopoverContent>
+// </Popover>
 )
 };
 
